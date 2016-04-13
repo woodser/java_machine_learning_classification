43860fba01baf554a7de85fafe00cd8abf0053fe
woodser <woodstae@gmail.com>
2016-04-13 09:34:42 -0400

    Added main for quick testing.

---
+++ src/main/Main.java
@@ -0,0 +1,38 @@
+package main;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import ml.Instance;
+import ml.MlUtils;
+import ml.Model;
+import ml.ModelLearner;
+
+public class Main {
+  
+  private static final String CSV_PATH = "resources/abalone.csv";
+  //private static final String CSV_PATH = "resources/iris_flowers.csv";
+  //private static final String CSV_PATH = "resources/salary.csv";
+
+  public static void main(String[] args) throws FileNotFoundException, IOException {
+    // load instances and train
+    List<Instance> instances = MlUtils.loadInstances(new File(CSV_PATH), false);
+    Model model = new ModelLearner();
+    model.addTrainingInstances(instances);
+    model.train();
+    
+    // test model performance
+    int right = 0;
+    int wrong = 0;
+    for (Instance instance : instances) {
+      Instance test = new Instance(instance.getFeatures());
+      model.classify(test);
+      //if (test.getProbability() < .75) continue;
+      if (test.getClassification().equals(instance.getClassification())) right++;
+      else wrong++;
+    }
+    System.out.println((double) right / (double) (right + wrong));
+  }
+}
